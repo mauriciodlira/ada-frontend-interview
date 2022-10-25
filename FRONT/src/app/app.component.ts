e48b1f8e889bf134a8ca525eb3bc6c7142c44d17
Mauricio de Lira <mauriciodlira@gmail.com>
2022-10-25 03:08:25 -0300

    Ajusta AppComponent

--- FRONT/src/app/app.component.ts
+++ FRONT/src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ApiFacadeService } from './shared/services/api-facade.service';
-import { CardManagementService } from './core/services/card-management.service';
-import { CardStatesEnum } from './shared/models/card-states.enum';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ada-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FRONT';
-  constructor(
-    private cardManagementService: CardManagementService,
-  ) {}
-
-  loadCards() {
-    this.cardManagementService.getCards().subscribe(x => console.log('x:', x));
-  }
-
-  postCard() {
-    this.cardManagementService.createCard('temp','temp content in markdown', CardStatesEnum.ToDo).subscribe(x => console.log('after creation: ', x));
-  }
-
+  constructor() { }
 }
